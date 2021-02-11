3133315eedc0e682f30b82cde3baac2fc1363ae0
Diego Oliva <dienrique24@hotmail.com>
2021-02-11 14:11:28 -0600

    added Tailwind

--- src/components/Navigation/NavigationItems/NavigationItem/NavItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavItem.js
@@ -1,7 +1,12 @@
-
+import styles from './NavItem.module.css';
+import { NavLink } from 'react-router-dom';
 
 const navitem = (props) => {
-    
+    return(
+        <li className={styles.NavItem}>
+            <NavLink to={props.link} exact activeClassName={styles.active} className={styles.NavItem}>{props.children}</NavLink>
+        </li>
+    )
 }
 
 export default navitem;
